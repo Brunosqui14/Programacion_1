15ab09883283b8654048e4cb76e5aa5456646dd4
Brunosqui14 <brunoquezada41@gmail.com>
2022-05-27 17:11:49 -0300

    loyd

--- loyd/main.js
+++ loyd/main.js
@@ -1,15 +1,36 @@
-function electrons(){
-    const datos = await fetch('https://fakestoreapi.com/docs')
-    const lista = await datos.json()
-    let items = []
-    lista.forEach(element => {
-        let fila = 
-        `<div>
-            
+const obtener_productos = async (dato) =>{
+
+    const datardos = await fetch("https://fakestoreapi.com/products/category/"+dato)
+    const elementos = await datardos.json()
+    let columnas = []
+    elementos.forEach(element => {
+        let columna = 
+        `<div class="col-lg-3">
+            <div class="card">
+                <img height="300" src="${element.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${element.title}</h5>
+                    <p class="card-text">$${element.price}</p>
+                    <a href="#" class="btn btn-primary">Comprar</a>
+                    <br></br>
+                    <p>
+                        <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample_${element.id}" aria-expanded="false" aria-controls="collapseWidthExample">
+                            Descripcion
+                        </button>
+                    </p>
+                    <div style="min-height: 120px;">
+                    <div class="collapse-horizontal collapse" id="collapseWidthExample_${element.id}" style="">
+                      <div class="card card-body" style="width: 300px;">
+                        ${element.description}
+                      </div>
+                    </div>
+                  </div>
+                  
+                </div>
+            </div>
         </div>`
-        filas.push(fila)
+        columnas.push(columna)
     });
-    document.getElementById("list_usuar").innerHTML = items.join('')
+    document.getElementById("catalogo").innerHTML = columnas.join('')
 }
-consumir_datos()
-}
+
