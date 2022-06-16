77622509ff7bdc6f8383bfe7fae2a1d4080a94f8
Brunosqui14 <brunoquezada41@gmail.com>
2022-06-16 17:10:44 -0300

    repaso 2

---
+++ Repaso_2/main.js
@@ -0,0 +1,15 @@
+const weas = async () => {
+    const datos_resibidos = await fetch('https://apis.datos.gob.ar/georef/api/provincias')
+    const provincias = await datos_resibidos.json()
+    let filas = []
+    provincias.forEach((element,index) => {
+        let fila = 
+        `<tr>
+            <td>${index+1}</td>
+            <td>${element.nombre}</td>
+        </tr>`
+        filas.push(fila)
+    });
+document.getElementById("id").innerHTML = filas.join('')
+}
+weas()
