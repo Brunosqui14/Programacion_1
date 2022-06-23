239e2ef799cf7531ab4384ea84d81fc36787d76e
Brunosqui14 <brunoquezada41@gmail.com>
2022-06-23 17:10:44 -0300

    fors

---
+++ Estructuras_repetitivas/main.js
@@ -0,0 +1,18 @@
+let numeros = [1,2,3,4,5,6,7,8,9,10]
+let filas = []
+numeros.forEach((element,index) => {
+    const res = element%2
+    if (res==0) {
+    const par = index+1
+    filas.push(par)
+}
+});
+console.log('forEach: '+filas)
+
+let forof =[]
+for (const numero of numeros) {
+    if (numero>2 && numero<9) {
+        forof.push(numeros)
+    }
+}
+console.log('forof: '+forof)
